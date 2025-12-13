62b6e42b055473120a0102ed5824689a97559915
rajasharmaa <151181564+rajasharmaa@users.noreply.github.com>
2025-12-13 22:09:29 +0530

    Modify CORS settings to add new origin
    Updated CORS configuration to include a new origin.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:3001','https://dttt-5.onrender.com/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -366,3 +366,4 @@ process.on('SIGINT', async () => {
   await closeDB();
   process.exit();
 });
+
