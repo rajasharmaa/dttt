f3112fa7fd97e8475450c1a26ec36d2dddfe5ed5
rajasharmaa <151181564+rajasharmaa@users.noreply.github.com>
2025-12-13 22:13:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:3001','https://dttt-5.onrender.com/'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:3001','https://dttt-5.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -366,4 +366,5 @@ process.on('SIGINT', async () => {
   await closeDB();
   process.exit();
 });
+
 
